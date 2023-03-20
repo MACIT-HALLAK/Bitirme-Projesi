211d4d4b5cb5a11e258cf81fb05b99d3a9f520be
MACIT-HALLAK <hlaqm8169@gmail.com>
2023-03-20 07:52:15 -0900

    Added a new components
    Added Title, Card, Book Categories and footer components.

---
+++ src/Components/BookCategories/BookCategories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './BookCategories.css';
+
+const BookCategories = () => {
+  return (
+    <aside>
+        <a href="#">kitab bolumleri</a>
+        <a href="#">islam dini</a>
+        <a href="#">tarih</a>
+        <a href="#">kultur</a>
+        <a href="#">pikoloj</a>
+        
+        
+           
+    </aside>  )
+}
+
+export default BookCategories
