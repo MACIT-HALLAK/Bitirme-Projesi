53aff646b89dd829cb1ea9998a40a18db040d9fc
ABDULLAH GHAZAL <omaargh001@gmail.com>
2023-04-05 15:33:07 +0300

    adding margin to search area

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Components/App";
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import Login from './Components/Login/Login'
-import Register from './Components/Register/Register';
-import Writer from './Components/Writer/Writer';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Writer from "./Components/Writer/Writer";
 import Writerspage from "./Components/Writerspage/Writerspage";
 import Seviyeler from "./Components/Seviyeler/Seviyeler";
 import BookPage from "./Components/BookPage/BookPage";
@@ -15,17 +15,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <Routes>
-      <Route path='/' element={<App />}/>
-      <Route path='/Anasayfa' element={<App />}/>
-      <Route path='/Login' element={<Login />}/>
-      <Route path='/Register' element={<Register />}/>
-      <Route path='/Yazarlar' element={<Writerspage />}/>
-      <Route path='/Seviyeler' element={<Seviyeler />}/>
-      <Route path='/Writer' element={<Writer />}/>
-      <Route path='/BookPage' element={<BookPage />}/>
-      <Route path='/ReadingPage' element={<ReadingPage />}/>
-    
-      
+      <Route path="/" element={<App />} />
+      <Route path="/Anasayfa" element={<App />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Yazarlar" element={<Writerspage />} />
+      <Route path="/Seviyeler" element={<Seviyeler />} />
+      <Route path="/Writer" element={<Writer />} />
+      <Route path="/BookPage" element={<BookPage />} />
     </Routes>
   </Router>
 );
