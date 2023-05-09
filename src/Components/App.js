a55e2b1ab0401c977dd801a7817fe6fda7227606
MACIT-HALLAK <hlaqm8169@gmail.com>
2023-05-09 12:21:30 -0900

    some picture has added

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,16 @@ import BookCategories from "./BookCategories/BookCategories";
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar/Navber";
 import Content from "./Content/Content";
-
+import book_img from "../Assets/images/si1.webp";
+import book_img1 from "../Assets/images/book1.webp";
+import book_img2 from "../Assets/images/book2.webp";
+import book_img3 from "../Assets/images/book3.webp";
+import book_img4 from "../Assets/images/si1.webp";
+import writer_Img from "../Assets/images/iyad.webp";
+import writer_Img1 from "../Assets/images/jalal.jpg";
+import writer_Img2 from "../Assets/images/mustafa.webp";
+import writer_Img3 from "../Assets/images/mutwali.webp";
+import writer_Img4 from "../Assets/images/si1.webp";
 import CardWriters from "./CardWriters/CardWriters";
 // import Form from "./Writer/Writer";
 
@@ -19,11 +28,11 @@ function App() {
       <section>
         <Title />
         <div className="parent">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card bookImage={book_img} writerImage={writer_Img}/>
+          <Card bookImage={book_img1} writerImage={writer_Img1}/>
+          <Card bookImage={book_img2} writerImage={writer_Img2}/>
+          <Card bookImage={book_img3} writerImage={writer_Img3}/>
+          <Card bookImage={book_img2} writerImage={writer_Img4}/>
         </div>
       </section>
       <BookCategories />
