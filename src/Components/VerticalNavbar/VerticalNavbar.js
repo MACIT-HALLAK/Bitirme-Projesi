16b5186220363349468534d1b5b3aa588fb43b5d
MACIT-HALLAK <hlaqm8169@gmail.com>
2023-06-03 06:10:17 -0900

    edit vertical navbar

--- src/Components/VerticalNavbar/VerticalNavbar.js
+++ src/Components/VerticalNavbar/VerticalNavbar.js
@@ -6,10 +6,25 @@ const VerticalNavbar = () => {
   return (
     <div className='vertical-navbar-layout'>
         <button>Reader At Work 2  <FaBook className='book-icon' /></button>
-        <button>Huckbery Finn Maceralari <FaBook className='book-icon' /></button>
+        <button>Huckbery Finn Maceralari  <FaBook className='book-icon' /></button>
         <button>قصص للأطفال <FaBook className='book-icon' /></button>
         <button>Alistirma kitabi <FaBook className='book-icon' /></button>
         <button>Reader At Work 1 <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
+        <button>Reader At Work <FaBook className='book-icon' /></button>
         <button>Reader At Work <FaBook className='book-icon' /></button>
         <button>Reader At Work <FaBook className='book-icon' /></button>
     </div>
