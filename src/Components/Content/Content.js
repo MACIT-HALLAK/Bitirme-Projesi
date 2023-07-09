50c136b1e20f8460d29db128a1147db509409dca
MACIT-HALLAK <hlaqm8169@gmail.com>
2023-07-09 12:58:29 -0900

    update content component

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./Content.css";
 import { Link } from "react-router-dom";
+import lib from '../../Assets/images/library.png'
 export default function Content() {
   return (
     <div className="content-main">
-      <div className="main-image">
-        <img src={require("../../Assets/images/Eyadprofile.jpg")}></img>
-      </div>
+     
       <div className="text-box">
-        <p>
-          lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-          lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-          lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-          lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-          lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-        </p>
+          <p>Merhaba Maced HALLAK</p>
+          <p>EJIYAL Kütüphanesine Hoşgeldiniz</p>
+       <img className="content-logo" src={lib} />
         <div className="footer-row">
           {/* <a className="btn btn-first">En Çok Okunan</a> */}
           <Link to='/ReadingPage' className="btn btn-second">En Çok Okunan</Link>
