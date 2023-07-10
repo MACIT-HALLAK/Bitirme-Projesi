1aa26c90314e7fb18a38c37cd4c250d7a3d0240e
MACIT-HALLAK <hlaqm8169@gmail.com>
2023-07-10 00:05:15 -0900

    in content i added a user name

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Content.css";
 import { Link } from "react-router-dom";
 import lib from '../../Assets/images/library.png'
+import axios from "axios";
+import { useCookies } from 'react-cookie';
+
 export default function Content() {
+
+  const [name,setName] = useState();
+  const [cookiesE, setCookiesE] = useCookies('email');
+
+  useEffect(()=>{
+    loadName();
+  },[])
+  const loadName = async()=>{
+   const res = await axios.get(`http://127.0.0.1:8000/api/getusername/${cookiesE.email}`)
+   setName(res.data);
+    console.log(res.data);
+  }
   return (
     <div className="content-main">
      
       <div className="text-box">
-          <p>Merhaba Maced HALLAK</p>
+        {name?<p>Merhaba <span className="content-name">{name}</span></p>:""}
+          
           <p>EJIYAL Kütüphanesine Hoşgeldiniz</p>
        <img className="content-logo" src={lib} />
         <div className="footer-row">
