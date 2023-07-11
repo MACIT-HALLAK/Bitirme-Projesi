a22ed2978d261f2df4d97ee77857d057c0c008ee
akka-sh <127871358+akka-sh@users.noreply.github.com>
2023-07-11 14:59:57 +0300

    ADD CategorieS

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Categories.css';
 //import { useCookies } from 'react-cookie';
 import Navbar from '../Navbar/Navber';
 import Footer from '../Footer/Footer';
 import { FaBook } from "react-icons/fa";
+import RestApi from '../../AppUrl/RestApi';
+import AppUrl from '../../AppUrl/AppUrl';
 
-const Categories = () => {
-const categories = [
-    'İslam Dini',
-    'Tarih',
-    'Kültür',
-    'Psikoloji',
-    'Kitaplar romanlar ve edebi hikayeler',
-    'Arap edebiyatı',
-    'Arapça dili',
-    'İslam felsefesi',
-    'Mühendislik',
-    'İktisat'
-];
+class Categories extends Component {
+    constructor(){
+        super();
+        this.state={
+            data:[]
+        }
+    }
 
-return (
-    <>
-    <div className='nav-1'>
-        <Navbar />
-    </div>
+    componentDidMount(){
+        RestApi.GetRequest(AppUrl.Categories).then(result => {
+            this.setState({ data: result });
+        });
+    }
 
-    <div className='categories'>
-        {categories.map((category, index) => (
-            <div className='cat-d' key={index}>
-                <p className='cat-p' >{category} <FaBook size={25} className='cat-icon' /></p>
+    render(){
+        const List = this.state.data;
+        const View = List.map(categories => (
+           <a href={`/categories/${categories.categorie}`}> <div className='cat-d' key={categories.id}>
+                <p className='cat-p'>{categories.categorie} <FaBook size={25} className='cat-icon' /></p>
+            </div></a>
+        ));
+
+        return (
+            <>
+            <div className='nav-1'>
+                <Navbar />
             </div>
-        ))}
-    </div>
-    
-    <Footer />
-    </>
-);
-};
+        
+            <div className='categories'>
+               {View}
+            </div>
+            
+            <Footer />
+            </>
+        );
+    }
+}
 
 export default Categories;
